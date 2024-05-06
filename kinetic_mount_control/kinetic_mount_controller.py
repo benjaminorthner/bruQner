a5efa15809a3e62ed61ee8256fd0b58035ed55bc
Benjamin Orthner <benjaminorthner@gmail.com>
2024-05-06 13:13:12 +0200

    new loading process

--- kinetic_mount_control/kinetic_mount_controller.py
+++ kinetic_mount_control/kinetic_mount_controller.py
@@ -23,15 +23,23 @@ class KineticMountController:
         try:
             return elliptec.scan_for_devices(self.controller, start_address=0, stop_address=0, debug=False)[0]
         except:
-            return None 
+            return None
+        
+    def _print_successfull_connection(self, device_info):
+        motor_type = device_info['info']['Motor Type']
+        description = elliptec.devices[motor_type]['description']
+        print(f"Device {sum(x is not None for x in self.assigned_device_infos)} succesfully connected ({description})")
 
     def __init__(self, number_of_devices):
-        
+
+        # initialise list of elliptec device objects
+        self.devices = []
+
         # Search for controller and assign it
         self._print_div('\nASSIGNING CONTROLLER')
         ports_found = elliptec.find_ports()
         print(f"Ports Found: {ports_found}")
-        self.controller = elliptec.Controller(ports_found[0])
+        self.controller = elliptec.Controller(ports_found[0], debug=False)
         self._print_div()
 
         # Go through sequence of searching for and assigning devices until all devices 
@@ -40,41 +48,74 @@ class KineticMountController:
         self._print_div('\nASSIGNING DEVICES')
 
         # Initialise list of assigned devices as empty
-        assigned_devices = [None] * number_of_devices 
+        self.assigned_device_infos = [None] * number_of_devices 
 
         # do initial check for how many devices are already assigned to somethign other than 0 
-        already_assigned_devices = elliptec.scan_for_devices(self.controller, start_address=1, stop_address=number_of_devices, debug=False)
-        if len(already_assigned_devices) > 0:
-            for device in already_assigned_devices:
+        already_assigned_device_infos = elliptec.scan_for_devices(self.controller, start_address=1, stop_address=number_of_devices, debug=False)
+        if len(already_assigned_device_infos) > 0:
+            for device in already_assigned_device_infos:
                 idx = int(device['info']['Address'], 16) - 1
-                assigned_devices[idx] = device
+                self.assigned_device_infos[idx] = device
+                self._print_successfull_connection(device)
         
-        # check if list is not full
-        if None in assigned_devices:
+        # while the not all devices have been assigned perform this loop
+        while None in self.assigned_device_infos:
             
             # check for device with address 0 (buffer address)
             try:
-                buffer_address_device = elliptec.scan_for_devices(self.controller, start_address=0, stop_address=0, debug=False)            
+                buffer_address_device = self._get_buffer_device()
+                assert buffer_address_device is not None
+
                 # Find lowest free slot
-                lowest_free_index = assigned_devices.index(None)
+                lowest_free_index = self.assigned_device_infos.index(None)
             except:
-                raise DeviceError('Less devices connected than specified')
+                # When buffer is empty ask user to plug in new device
+                input('Connect next device (only 1) then press "Enter" to continue...')
+                sys.stdout.write("\033[F")  # Move the cursor to the previous line
+                sys.stdout.write("\033[K")  # clear the line
+                sys.stdout.flush()
+                time.sleep(6) # wait for connection to actually happen
+                continue
 
             # assign corresponding address to this device (convert from dec to hex to str)
-            device = elliptec.Motor(self.controller, address='0')
+            device = elliptec.Motor(self.controller, address='0', debug=False)
             device.change_address(str(hex(lowest_free_index+1)[2:]).upper())
-            assigned_devices[lowest_free_index] = buffer_address_device[0]
+            self.assigned_device_infos[lowest_free_index] = buffer_address_device
+            self._print_successfull_connection(buffer_address_device)
+
         
-        # if list of assigned devices is fully populated
-        else:
-            # check if there are more devices in the 0 slot
-            if self._get_buffer_device() is not None:
-                raise DeviceError('More devices connected than specified')
+        # Now the entire list of devices must be populated
+        # check if there are more devices in the 0 slot. And if yes raise exception
+        # because they should have all been assigned by now
+        if self._get_buffer_device() is not None:
+            raise DeviceError('More devices connected than specified')
 
-        for device in assigned_devices:
-            print(device)
+        self._print_div()
 
+        # Initialise elliptec objects for each device (based on motor type)
+        for device in self.assigned_device_infos:
+            
+            motor_type = device['info']['Motor Type']
+            address = device['info']['Address']
+
+            # Rotation Mount
+            if motor_type == 14:
+                self.devices.append(elliptec.Rotator(self.controller, address=address, debug=False))
+            
+            # Dual Position Slider
+            elif motor_type == 6:
+                self.devices.append(elliptec.Shutter(self.controller, address=address, debug=False))
+
+            # Raise error if anything else
+            else:
+                assert motor_type in [6, 14], "\n\nERROR: Unsopported Device Type\n"
 
 
 if __name__ == '__main__':
-    KMC = KineticMountController(number_of_devices=2)
+    KMC = KineticMountController(number_of_devices=3)
+    
+    shutter, a_hwp, b_hwp = KMC.devices
+
+    shutter.home()
+    shutter.open()
+
